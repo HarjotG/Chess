c7a2821a4ce6bcc9dacef2e857ef46303c3d386d
Harjot Grewal <harjot123g@gmail.com>
2021-08-19 15:54:47 -0700

    added images

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: `${__dirname}/src/index.tsx`,
   output: {
     path: `${__dirname}/build`,
-    publicPath: '/chess/build/',
+    publicPath: '/Chess/build/',
     filename: 'bundle.js',
   },
 
@@ -45,7 +45,7 @@ module.exports = {
   // and test spa-github-pages redirect in dev
   devServer: {
     historyApiFallback: {
-      rewrites: [{ from: /\/chess\/[^?]/, to: '/404.html' }]
+      rewrites: [{ from: /\/Chess\/[^?]/, to: '/404.html' }],
     },
   },
 };
